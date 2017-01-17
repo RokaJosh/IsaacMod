a2c2672e0f6c64ad2bd4aa3fc4f599db971e965e
RokaJosh <J2MuchContact@gmail.com>
2017-01-17 18:06:16 +0000

    The Shart and Beggar's Hope
    2 New Items, Unsprited

--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 local discord = RegisterMod("Discord Mod", 1)
 local game = Game()
-local room = Room()
 local frameCounter = 0;
 local lockPosition = false;
+
 
 --Items--
 local legacy_item = Isaac.GetItemIdByName( "Dad's Legacy" )
 local krampus_horn = Isaac.GetItemIdByName( "Krampuses Horn" )
+local shart = Isaac.GetItemIdByName( "The Shart" )
+local bhope = Isaac.GetItemIdByName("Beggar's Hope")
 local philID = Isaac.GetCardIdByName("Philosopher's Stone")
 
 --Timing--
@@ -25,8 +27,7 @@ end
 
 --Card Effects--
 function discord:CardCallBack(cardId)
-	if cardId == philID then
-		
+	--if cardId == philID then	
 end
 
 --Passive Items--
@@ -42,6 +43,7 @@ function discord:cache(p, flag)
 end
 
 --Active Items--
+--Krampuses Horn--
 function discord:use_krampus_horn()
     local player = Isaac.GetPlayer(0)
     local entities = Isaac.GetRoomEntities()
@@ -56,7 +58,39 @@ function discord:use_krampus_horn()
 return true
 end
 
+--The Shart--
+function discord:use_shart()
+	local chance = math.random(1, 2)
+	if chance == 1 then
+		for i = 1, 3 do
+			local player = Isaac.GetPlayer(0)
+			local pos = Isaac.GetFreeNearPosition(player.Position, 80.0);
+			Isaac.GridSpawn(GridEntityType.GRID_POOP, 0, pos, false);
+		end
+	end
+	if chance == 2 then
+		local player = Isaac.GetPlayer(0)
+		local pos = player.Position
+		game.ButterBeanFart (game, pos, 3, player, true)
+	end
+return true
+end
+
+--Beggar's Hope--
+function discord:use_bhope()
+	local room = Game():GetRoom()
+	local coinNum = room:GetAliveEnemiesCount();
+	local player = Isaac.GetPlayer(0)
+	for i = 1, coinNum do
+		pos = Isaac.GetFreeNearPosition(player.Position, 80.0);
+		Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COIN, CoinSubType.COIN_PENNY, pos, Vector(0, 0), Isaac.GetPlayer(0));
+	end
+return true
+end
+
 --Callbacks--
 discord:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, discord.cache);
 discord:AddCallback(ModCallbacks.MC_USE_ITEM, discord.use_krampus_horn, krampus_horn);
+discord:AddCallback(ModCallbacks.MC_USE_ITEM, discord.use_shart, shart);
+discord:AddCallback(ModCallbacks.MC_USE_ITEM, discord.use_bhope, bhope);
 discord:AddCallback(ModCallbacks.MC_POST_UPDATE, discord.updateFrame);
